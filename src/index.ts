5bc0bed6d9a57b0eb5534399fdb7803cdd8c1776
AlaguSelvan <alaguselvan97@gmail.com>
2019-11-04 10:24:36 +0530

    interface and implements

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-// const user = new User();
-// const company = new Company();
+const user = new User();
+const customMap = new CustomMap('map');
+const company = new Company()
 
-// console.log(user);
-// console.log(company);
-
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+customMap.addMarker(user)
+customMap.addMarker(company)
